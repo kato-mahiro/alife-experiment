690515793c4dd7c9b9c72c9ec80eaef9d89e9cc4
katomasahiro10 <katomasahiro10@gmail.com>
2020-05-11 13:42:04 +0900

    use accident_task.py instead of task.py

--- experiment.py
+++ experiment.py
@@ -5,11 +5,11 @@ import sys
 
 from setting import *
 from modneat.modneat import agents
-from task import Task_without_accident
-from task import Task_with_accident
+from accident_task import Task_without_accident
+#from accident_task import Task_with_accident
 
-agents = agents.Agents('ExHebbianNetwork',POPULATION_SIZE,False,input_num=8,output_num=3, connection_num_lower_limit=40,connection_num_upper_limit=100)
-is_accident = True
+agents = agents.Agents('ExHebbianNetwork',POPULATION_SIZE,is_automatic_change = False,input_num=3,output_num=3, connection_num_lower_limit=10,connection_num_upper_limit=100)
+is_accident = False
 
 for gen in range(GENERATION_NUM):
     print('===')
@@ -21,25 +21,20 @@ for gen in range(GENERATION_NUM):
         task = Task_with_accident()
 
     for pop in range(POPULATION_SIZE):
-        agents[pop].fitness = task.dotask(agents[pop])
+        agents[pop].fitness = task.execute_task(agents[pop])
 
     print('max fitness:', agents.max_fitness)
     print('average fitness:', agents.average_fitness)
     print('minimum fitness: ', agents.min_fitness)
 
-    if(agents.average_fitness > 52.0):
-        is_accident = True
+    if(agents.average_fitness >= 9.1):
+        print('Successfully evolved')
+        agents.sort(key=attrgetter('fitness'), reverse =True)
+        agents[0].show_network()
+        sys.exit()
 
     agents.sort(key=attrgetter('fitness'), reverse =True)
-    print(agents[0].histories[0])
-    print(agents[0].histories[1])
-    print(agents[0].histories[2])
-    print(agents[0].histories[3])
-    print(agents[0].histories[4])
-    print(agents[0].histories[5])
-
-
-    agents = agents.evolution_mgg(task=task.dotask,elite_num=2, mutate_prob=0.05, sigma=0.1)
+    agents = agents.evolution_mgg(task=task.execute_task,elite_num=2, mutate_prob=0.05, sigma=0.1)
 
 name = sys.argv[1] + '.pickle'
 agents.save_agents(name)
