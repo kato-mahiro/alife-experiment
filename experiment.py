c6252dd2a8daeec60eea0a6fe4a32330a8b13581
katomasahiro10 <katomasahiro10@gmail.com>
2020-05-11 16:07:48 +0900

    use test_agent
    execute_task の代わりに test_agentを使用する
    タスクが簡単すぎると進化が促進されない

--- experiment.py
+++ experiment.py
@@ -22,21 +22,21 @@ for gen in range(GENERATION_NUM):
     
     for pop in range(POPULATION_SIZE):
         task.change_rule()
-        agents[pop].fitness = task.execute_task(agents[pop])
+        agents[pop].fitness = task.test_agent(agents[pop])
 
     print('max fitness:', agents.max_fitness)
     print('average fitness:', agents.average_fitness)
     print('minimum fitness: ', agents.min_fitness)
 
-    if(agents.average_fitness >= 9.1):
+    if(agents.average_fitness >= 8.1):
         print('Successfully evolved')
         agents.sort(key=attrgetter('fitness'), reverse =True)
-        task.test_agent(agents[0])
+        task.test_agent(agents[0],True)
         agents[0].show_network()
         sys.exit()
 
     agents.sort(key=attrgetter('fitness'), reverse =True)
-    agents = agents.evolution_mgg(task=task.execute_task,elite_num=2, mutate_prob=0.05, sigma=0.1)
+    agents = agents.evolution_mgg(task=task.test_agent,elite_num=2, mutate_prob=0.05, sigma=0.1)
 
 name = sys.argv[1] + '.pickle'
 agents.save_agents(name)
